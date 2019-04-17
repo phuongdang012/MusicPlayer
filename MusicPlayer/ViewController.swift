0277fed4684d47220325d418fa441d2860599d85
Phương Đăng Trần Thành <phuongdang012@gmail.com>
2019-04-17 19:19:18 +0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Trần Thành Phương Đăng on 17/04/19.
+//  Copyright © 2019 phuongdang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
